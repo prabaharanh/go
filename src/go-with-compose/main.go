8638e64390acb33db399b9b0b7b451cb8dd58923
Prabaharan Haridass <prabaharan.haridass@outlook.com>
2018-07-25 21:27:56 +1200

    done

--- src/go-with-compose/main.go
+++ src/go-with-compose/main.go
@@ -1,121 +1,52 @@
-// Our first program will print the classic "hello world"
-// message. Here's the full source code.
-// Go offers built-in support for JSON encoding and
-// decoding, including to and from built-in and custom
-// data types.
-
 package main
 
-import "encoding/json"
+import "database/sql"
 import "fmt"
-import "os"
+import "time"
+import "github.com/lib/pq"
 
-// We'll use these two structs to demonstrate encoding and
-// decoding of custom types below.
-type response1 struct {
-    Page   int
-    Fruits []string
-}
-type response2 struct {
-    Page   int      `json:"page"`
-    Fruits []string `json:"fruits"`
-}
+const (
+    DB_USER     = "postgres"
+    DB_PASSWORD = "test123"
+    DB_NAME     = "test"
+)
 
 func main() {
+    dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+    db, err := sql.Open("postgres", dbinfo)
+    checkErr(err)
+    defer db.Close()
 
-    // First we'll look at encoding basic data types to
-    // JSON strings. Here are some examples for atomic
-    // values.
-    bolB, _ := json.Marshal(true)
-    fmt.Println(string(bolB))
+    fmt.Println("# Inserting values")
 
-    intB, _ := json.Marshal(1)
-    fmt.Println(string(intB))
+    var lastInsertId int
+    err = db.QueryRow("INSERT INTO employees(a,b) VALUES(1,"a") returning uid;", "astaxie", "研发部门", "2012-12-09").Scan(&lastInsertId)
+    checkErr(err)
+    fmt.Println("last inserted id =", lastInsertId)
 
-    fltB, _ := json.Marshal(2.34)
-    fmt.Println(string(fltB))
+    fmt.Println("# Updating")
+    stmt, err := db.Prepare("update userinfo set username=$1 where uid=$2")
+    checkErr(err)
 
-    strB, _ := json.Marshal("gopher")
-    fmt.Println(string(strB))
+    res, err := stmt.Exec("astaxieupdate", lastInsertId)
+    checkErr(err)
 
-    // And here are some for slices and maps, which encode
-    // to JSON arrays and objects as you'd expect.
-    slcD := []string{"apple", "peach", "pear"}
-    slcB, _ := json.Marshal(slcD)
-    fmt.Println(string(slcB))
+    affect, err := res.RowsAffected()
+    checkErr(err)
 
-    mapD := map[string]int{"apple": 5, "lettuce": 7}
-    mapB, _ := json.Marshal(mapD)
-    fmt.Println(string(mapB))
+    fmt.Println(affect, "rows changed")
 
-    // The JSON package can automatically encode your
-    // custom data types. It will only include exported
-    // fields in the encoded output and will by default
-    // use those names as the JSON keys.
-    res1D := &response1{
-        Page:   1,
-        Fruits: []string{"apple", "peach", "pear"}}
-    res1B, _ := json.Marshal(res1D)
-    fmt.Println(string(res1B))
+    fmt.Println("# Querying")
+    rows, err := db.Query("SELECT * FROM employees")
+    checkErr(err)
 
-    // You can use tags on struct field declarations
-    // to customize the encoded JSON key names. Check the
-    // definition of `response2` above to see an example
-    // of such tags.
-    res2D := &response2{
-        Page:   1,
-        Fruits: []string{"apple", "peach", "pear"}}
-    res2B, _ := json.Marshal(res2D)
-    fmt.Println(string(res2B))
-
-    // Now let's look at decoding JSON data into Go
-    // values. Here's an example for a generic data
-    // structure.
-    byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
-
-    // We need to provide a variable where the JSON
-    // package can put the decoded data. This
-    // `map[string]interface{}` will hold a map of strings
-    // to arbitrary data types.
-    var dat map[string]interface{}
-
-    // Here's the actual decoding, and a check for
-    // associated errors.
-    if err := json.Unmarshal(byt, &dat); err != nil {
-        panic(err)
+    for rows.Next() {
+        var uid int
+        var username string
+        var department string
+        var created time.Time
+        err = rows.Scan(&uid, &username, &department, &created)
+        checkErr(err)
+        fmt.Println("uid | username  | created ")
+        fmt.Printf("%3v | %8v | %6v | %6v\n", uid, username, created)
     }
-    fmt.Println(dat)
-
-    // In order to use the values in the decoded map,
-    // we'll need to cast them to their appropriate type.
-    // For example here we cast the value in `num` to
-    // the expected `float64` type.
-    num := dat["num"].(float64)
-    fmt.Println(num)
-
-    // Accessing nested data requires a series of
-    // casts.
-    strs := dat["strs"].([]interface{})
-    str1 := strs[0].(string)
-    fmt.Println(str1)
-
-    // We can also decode JSON into custom data types.
-    // This has the advantages of adding additional
-    // type-safety to our programs and eliminating the
-    // need for type assertions when accessing the decoded
-    // data.
-    str := `{"page": 1, "fruits": ["apple", "peach"]}`
-    res := response2{}
-    json.Unmarshal([]byte(str), &res)
-    fmt.Println(res)
-    fmt.Println(res.Fruits[0])
-
-    // In the examples above we always used bytes and
-    // strings as intermediates between the data and
-    // JSON representation on standard out. We can also
-    // stream JSON encodings directly to `os.Writer`s like
-    // `os.Stdout` or even HTTP response bodies.
-    enc := json.NewEncoder(os.Stdout)
-    d := map[string]int{"apple": 5, "lettuce": 7}
-    enc.Encode(d)
-}
